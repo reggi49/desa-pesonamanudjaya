cd1009d06180c0fc35dd24994680dc148f1685bf
reggi49 <reggimuhamad@gmail.com>
2024-03-23 12:27:22 +0700

    fix ggallery css7

--- application/views/galeri/foto.php
+++ application/views/galeri/foto.php
@@ -2,19 +2,19 @@
     <div class="container">
         <div class="row">
 
-            <div class="gallery">
+            <div class="gallery span7">
                 <?php if (!empty($gallery)) { ?>
                     <?php
                     foreach ($gallery as $row) {
                         $uploadDir = base_url() . 'upload_file/images/';
                         $imageURL = $uploadDir . $row["galery"];
                     ?>
-                        <div class="span8">
+                        <!-- <div class="span8"> -->
                             <a href="<?php echo $imageURL; ?>" data-fancybox="gallery" data-caption="<?php echo $row["title"]; ?>">
                                 <img src="<?php echo $imageURL; ?>" alt="" />
                                 <p><?php echo $row["title"]; ?></p>
                             </a>
-                        </div>
+                        <!-- </div> -->
                     <?php } ?>
                 <?php } else { ?>
                     <div class="span8">
