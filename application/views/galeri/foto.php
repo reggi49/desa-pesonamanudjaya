f959bdead436292ebc1ded0c005f6a22a8e8451e
reggi49 <reggimuhamad@gmail.com>
2024-03-23 11:32:52 +0700

    fix ggallery frontend3

--- application/views/galeri/foto.php
+++ application/views/galeri/foto.php
@@ -22,8 +22,6 @@
                     </div>
                 <?php } ?>
             </div>
-        </div>
-    </div>
 
         <!-- Fancybox CSS library -->
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fancybox/disc/jquery.fancybox.min.css'); ?>">
